89cce8476604bd333dee1e9897428af9b4a1b087
Eric Cheyssial <147110131+echeys01@users.noreply.github.com>
2024-05-03 06:47:06 -0700

    Add files via upload

---
+++ scripts/event_info.js
@@ -0,0 +1,22 @@
+const backBtn = document.getElementById("returnButton");
+backBtn.addEventListener("click", returnClicked);
+
+const join = document.getElementById("joinEventButton");
+join.addEventListener("click", joinClicked);
+
+function returnClicked() {
+    window.location.replace("volunteer_event_page.html");
+}
+
+function joinClicked() {
+    // Add user if not already joined event.
+
+}
+
+
+
+
+
+
+
+
